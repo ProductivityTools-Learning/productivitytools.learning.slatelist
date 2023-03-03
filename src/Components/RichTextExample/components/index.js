799f65d8ba32fd71d5c040afbd0614b4aa0e4912
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-03-03 16:57:04 +0100

    rich text

--- src/Components/RichTextExample/components/index.js
+++ src/Components/RichTextExample/components/index.js
@@ -3,6 +3,47 @@ import ReactDOM from 'react-dom'
 import { cx, css } from '@emotion/css'
 
 
+export const Button = React.forwardRef(
+    (
+        {
+            className, active, reversed, ...props
+        }, ref) => (
+        <span
+            {...props}
+            ref={ref}
+            className={cx(
+                className,
+                css`
+                  cursor: pointer;
+                  color: ${reversed
+                        ? active
+                            ? 'white'
+                            : '#aaa'
+                        : active
+                            ? 'black'
+                            : '#ccc'};
+                `
+            )} />
+    )
+)
+
+export const Icon = React.forwardRef(
+    ({ className, ...props }, ref) => (
+        <span
+            {...props}
+            ref={ref}
+            className={cx(
+                'material-icons',
+                className,
+                css`
+                  font-size: 18px;
+                  vertical-align: text-bottom;
+                `
+            )}
+        />
+    )
+)
+
 export const Menu = React.forwardRef(({ className, ...props }, ref) => (
     <div {...props}
         ref={ref}
@@ -31,7 +72,7 @@ export const Toolbar = React.forwardRef(({ className, ...props }, ref) =>
               position: relative;
               padding: 1px 18px 17px;
               margin: 0 -20px;
-              border-bottom: 2px solid #eee;
+              border-bottom: 2px solid black;
               margin-bottom: 20px;
             `
         )}
--- src/Components/RichTextExample/index.js
+++ src/Components/RichTextExample/index.js
@@ -7,18 +7,87 @@ import {
     Descendant,
     Element as SlateElement,
 } from 'slate'
-import { Toolbar } from './components'
+import { Toolbar, Button, Icon } from './components'
 
-
+const LIST_TYPES = ['numbered-list', 'bulleted-list']
+const TEXT_ALIGN_TYPES = ['left', 'center', 'right', 'justify']
 const RichTextExample = () => {
+    const renderElement = useCallback(props => <Element {...props} />, [])
 
     const editor = useMemo(() => withReact(createEditor()), [])
 
 
     return (<Slate editor={editor} value={initialValue}>
-        <Toolbar></Toolbar>
-        <Editable></Editable>
+        <Toolbar>
+            <BlockButton format="heading-one" icon="looks_one" />
+        </Toolbar>
+        <Editable
+            renderElement={renderElement}
+        ></Editable>
     </Slate>)
+}
+
+const Element = ({ attribites, children, element }) => {
+    debugger;
+    switch (element.type) {
+        case 'heading-one':
+            return (<h1 {...attribites}>
+                {children}
+            </h1>)
+        default:
+            return <span {...attribites}>{children}</span>
+    }
+}
+
+const BlockButton = ({ format, icon }) => {
+    const editor = useSlate();
+    return (
+        <Button
+            onMouseDown={event => {
+                event.preventDefault();
+                toggleBlock(editor, format);
+            }}
+        >
+            <Icon>{icon}</Icon>
+        </Button>
+    )
+}
+
+const toggleBlock = (editor, format) => {
+    debugger;
+    const isActive = isBlockActive(editor, format, TEXT_ALIGN_TYPES.includes(format) ? 'align' : 'type')
+    const isList = LIST_TYPES.includes(format);
+
+    Transforms.unwrapNodes(editor, {
+        match: n => !Editor.isEditor(n) &&
+            SlateElement.isElement(n) &&
+            LIST_TYPES.includes(n.type) &&
+            !TEXT_ALIGN_TYPES.includes(format),
+        split: true
+    })
+    let newProperties = {
+        type: isActive ? 'paragraph' : isList ? 'list-item' : format,
+    }
+
+    Transforms.setNodes(editor, newProperties);
+}
+
+const isBlockActive = (editor, format, blockType = 'type') => {
+    const { selection } = editor
+    if (!selection) return false;
+    let editorNodes = Editor.nodes(editor, {
+        at: Editor.unhangRange(editor, selection),
+        match: n =>
+            !Editor.isEditor(n) &&
+            SlateElement.isElement(n) &&
+            n[blockType] == format,
+    })
+    debugger;
+    console.log(editorNodes);
+
+    const [match] = Array.from(editorNodes)
+
+    return !!match;
 }
 
 const initialValue = [
