5980aef6b5cea435b3f5a98212cdfe598e84ea1a
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-03-03 10:53:19 +0100

    slate basic example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
 
+import React, { useState } from 'react'
+import { createEditor } from 'slate';
+import { Slate, Editable, withReact } from 'slate-react'
+
+const initialValue = [
+  {
+    type: 'paragraph',
+    children: [{ text: 'A line of text in a paragraph.' }],
+  },
+]
+
 function App() {
+
+
+  const [editor] = useState(() => withReact(createEditor()))
+
   return (
     <div className="App">
-      hello
+      <Slate editor={editor} value={initialValue} >
+        <Editable />
+      </Slate>
     </div>
   );
 }
